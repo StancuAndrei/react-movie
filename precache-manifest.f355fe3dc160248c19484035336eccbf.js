be9a20eca17412692e5091c86372946c87af0e07
StancuAndrei <stancuandrei18@yahoo.com>
2020-07-04 14:39:41 +0300

    Updates

---
+++ precache-manifest.f355fe3dc160248c19484035336eccbf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819f011ba181795ddd444664f3f33887",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "8e8aeeb9827ae61042f6",
+    "url": "/react-movie/static/css/2.3c357db0.chunk.css"
+  },
+  {
+    "revision": "07dcb0f3d622f2c580d3",
+    "url": "/react-movie/static/css/main.509f0a63.chunk.css"
+  },
+  {
+    "revision": "8e8aeeb9827ae61042f6",
+    "url": "/react-movie/static/js/2.f560f136.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-movie/static/js/2.f560f136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07dcb0f3d622f2c580d3",
+    "url": "/react-movie/static/js/main.88ef99f3.chunk.js"
+  },
+  {
+    "revision": "cb0d2286c051b889083c",
+    "url": "/react-movie/static/js/runtime-main.6d0db180.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/react-movie/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/react-movie/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/react-movie/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/react-movie/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-movie/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-movie/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-movie/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-movie/static/media/slick.f97e3bbf.svg"
+  }
+]);
